05a7d254ee113d5f54f4988ff823169cd3724e7c
Devansh Rathor <devanshrathor123@gmail.com>
2023-12-27 22:28:33 +0530

    Refactor product comparison page and update product thumbnail URLs

--- shopping-assistant/app/compare/[id]/page.tsx
+++ shopping-assistant/app/compare/[id]/page.tsx
@@ -7,6 +7,21 @@ import { Product } from "@/components/Products";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+  source: string;
+};
+
 const ComparePage = ({ params: { id } }: { params: { id: number } }) => {
   const product = products.find((product) => product.id === +id);
   const router = useRouter();
@@ -29,88 +44,95 @@ const ComparePage = ({ params: { id } }: { params: { id: number } }) => {
     source,
   } = product;
 
+  const compare = products.find(
+    (product) =>
+      product.id !== +id &&
+      product.title === title &&
+      product.source !== source,
+  );
   return (
-    <div className="grid max-h-[32rem] gap-8 overflow-y-auto px-4">
-      <div className="grid">
-        <div className="flex h-64 w-64 flex-col items-center justify-center place-self-center">
-          <Image
-            src={thumbnail}
-            alt={title}
-            className="h-full w-full object-cover"
-            height={500}
-            width={500}
-          />
-        </div>
-        <div>
-          <h1 className="text-3xl font-bold">{title}</h1>
-          <p className="mb-4 text-sm font-bold text-gray-400">
-            {brand} | {category}
-          </p>
-          {discountPercentage ? (
-            <div className="mb-4 flex items-center">
-              <p className="text-2xl font-bold text-primary">
-                £{(price * (1 - discountPercentage / 100)).toFixed(2)}
-              </p>
-              <p className="ml-4 text-xl font-bold text-gray-400 line-through">
-                £{price}
-              </p>
-            </div>
-          ) : (
-            <p className="text-2xl font-bold text-red-600">£{price}</p>
-          )}
-
-          <p className="mb-4 text-xl font-bold text-gray-400">
-            Rating {rating} / 5
-          </p>
-
-          <p className="text-muted-foreground">{description}</p>
-
-          {images.length > 0 && (
-            <div className="mt-4 grid grid-cols-4 gap-4">
-              {images.map((image) => (
-                <Image
-                  key={image}
-                  src={image}
-                  alt={title}
-                  className="h-32 w-32 object-contain"
-                  height={128}
-                  width={128}
-                />
-              ))}
-            </div>
-          )}
-
-          <p className="text-xl font-bold text-gray-400">
-            Source:{" "}
-            {source === "A" ? (
-              <Button variant="link">
-                <Link href="amazon.com">Amazon</Link>
-              </Button>
-            ) : (
-              <Button variant="link">
-                <Link href="ebay.com">eBay</Link>
-              </Button>
-            )}
-          </p>
-
-          <Button
-            className="w-full"
-            variant="default"
-            onClick={() => console.log("add to cart")}
-          >
-            Add to cart
-          </Button>
-        </div>
-      </div>
+    <div className="grid max-h-[32rem] grid-cols-2 gap-8 overflow-y-auto px-4">
+      <ProductCompareView {...product} />
+      {compare && <ProductCompareView {...compare} />}
     </div>
   );
 };
 
-const ProductCompareView = ({ id }: { id: number }) => {
+const ProductCompareView = ({
+  id,
+  title,
+  description,
+  price,
+  discountPercentage,
+  rating,
+  stock,
+  brand,
+  category,
+  thumbnail,
+  images,
+  source,
+}: Product) => {
   return (
-    <div>
-      <h1>Compare Prices</h1>
-      <p>Product ID: {id}</p>
+    <div className="grid">
+      <div className="flex h-64 w-64 flex-col items-center justify-center place-self-center">
+        <Image
+          src={thumbnail}
+          alt={title}
+          className="h-full w-full object-cover"
+          height={500}
+          width={500}
+        />
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <p className="mb-4 text-sm font-bold text-gray-400">
+          {brand} | {category}
+        </p>
+        {discountPercentage ? (
+          <div className="mb-4 flex items-center">
+            <p className="text-2xl font-bold text-primary">
+              £{(price * (1 - discountPercentage / 100)).toFixed(2)}
+            </p>
+            <p className="ml-4 text-xl font-bold text-gray-400 line-through">
+              £{price}
+            </p>
+          </div>
+        ) : (
+          <p className="text-2xl font-bold text-red-600">£{price}</p>
+        )}
+
+        <p className="mb-4 text-xl font-bold text-gray-400">
+          Rating {rating} / 5
+        </p>
+
+        <p className="text-muted-foreground">{description}</p>
+
+        <p className="text-xl font-bold text-gray-400">
+          Source:{" "}
+          {source === "A" ? (
+            <Button variant="link">
+              <Link href="amazon.com">Amazon</Link>
+            </Button>
+          ) : (
+            <Button variant="link">
+              <Link href="ebay.com">eBay</Link>
+            </Button>
+          )}
+        </p>
+
+        <Button
+          className="w-full"
+          variant="default"
+          onClick={() => console.log("add to cart")}
+        >
+          Add to cart
+        </Button>
+        <Link href={`/product/${id}`}>
+          <Button className="\ mt-4 w-full" variant="outline">
+            View Product
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
