e1fe95343d3ef8424e9daaf2c812bd7b9171df54
Abhay Mathur <a21.mathur21@gmail.com>
2023-12-27 23:06:12 +0530

    Update Categories.tsx

--- shopping-assistant/components/Categories.tsx
+++ shopping-assistant/components/Categories.tsx
@@ -16,7 +16,7 @@ const Categories = () => {
 
   const handleSearch = async () => {
     try {
-      await fetch("http://localhost:5000/search", {
+      fetch("http://localhost:5000/search", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ search }),
